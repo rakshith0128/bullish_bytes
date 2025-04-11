f10cac8d4d5ae286181148cd5e6101e4bc59f9e6
rakshith <rakshith.s2004@gmail.com>
2025-04-11 11:16:29 +0000

    Fix: Import error in App.tsx
    The import of `Toaster` from `@/hooks/use-toast` is incorrect. The correct import should be from `@/components/ui/toast`.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { Toaster } from "@/hooks/use-toast";
+import { ToastProvider as Toaster } from "@/components/ui/toast";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
